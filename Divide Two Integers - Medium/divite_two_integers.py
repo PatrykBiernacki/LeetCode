0c2b2ffe14c30775c39532cf01e8cd90ce46bf71
Patryk Biernacki <patryk.biernacki11@gmail.com>
2023-12-20 21:50:55 +0100

    divide_two_intigers bug fixed on quotient limits

--- Divide Two Integers - Medium/divite_two_integers.py
+++ Divide Two Integers - Medium/divite_two_integers.py
@@ -12,15 +12,17 @@ class Solution:
         if divisor < 0:
             non_negative *= -1  
             divisor *= -1
+
         quotient = len(range(0, dividend-divisor+1, divisor))
         
         if quotient >= 2**31-1 and non_negative == 1:
             return 2**31-1
-        if quotient >= 2**31-1 and non_negative == -1:
+        if quotient >= 2**31 and non_negative == -1:
             return -2**31
         return quotient * non_negative
 
 
 if __name__ == '__main__':
     solution1 = Solution()
-    print(solution1.divide(-2147483648, -1))
+    print(solution1.divide(-2147483648, 1))
+    print(solution1.divide(2147483647, -1))
--- sandbox.py
+++ sandbox.py
@@ -1,15 +1,4 @@
 '''script for testing functionality'''
-# nums = [0,1,2,3,4,5,5,-5]
-# nums.sort()
-# print(nums)
 
-positives = {10:1, 12:1}
-negatives = {-10:1, -12:1}
-triplets = set()
-
-for positiv in positives:
-    if 0 - positiv in negatives:
-        add_list = [0 - positiv, 0, positiv]
-        triplets.add(tuple(add_list))
-
-print (triplets)
+print (-2**31 +1)
+print (2**31-1)
