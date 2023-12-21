4b5e2067b0edc36fd660bef7402bd87b195fddbf
Patryk Biernacki <patryk.biernacki11@gmail.com>
2023-12-21 11:19:58 +0100

    second solution to permutations, using helper function.

--- Permutations - Medium/permutations.py
+++ Permutations - Medium/permutations.py
@@ -1,11 +1,24 @@
 from itertools import permutations
 
 class Solution:
-    # def permute_oneliner(self, nums: list[int]) -> list[list[int]]:
-    #     return set(permutations(nums))
+    def permute_oneliner(self, nums: list[int]) -> list[list[int]]:
+        return set(permutations(nums))
 
     def permute(self, nums: list[int]) -> list[list[int]]:
-        return set(permutations(nums))
+        
+        answer_set = set()
+
+        def helper(nums, path):
+            if not nums:
+                answer_set.add(tuple(path))
+                return
+            for i in range(len(nums)):
+                helper(nums[:i] + nums[i+1:], path + [nums[i]]) 
+        
+        helper(nums, []) 
+
+        return answer_set
+    
 
 
 if __name__ == '__main__':
@@ -13,4 +26,4 @@ if __name__ == '__main__':
     print(solution1.permute([1,2,3]))
     print(solution1.permute([0,1]))
     print(solution1.permute([1]))
-    print(solution1.permute([1,2,3,4,5,6]))
+    print(solution1.permute([1,2,3,4]))
--- sandbox.py
+++ sandbox.py
@@ -1,8 +1,2 @@
 '''script for testing functionality'''
 
-nums = [1,2,3,4,5,6]
-new_nums = set(nums)
-
-nums.pop()
-print(nums)
-print(new_nums)
