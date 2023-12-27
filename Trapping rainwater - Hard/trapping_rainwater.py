47d50cca56f2242747b8b892595610a3e881cf20
Patryk Biernacki <patryk.biernacki11@gmail.com>
2023-12-27 09:18:56 +0100

    trapping_rainwater started script, ongoing

---
+++ Trapping rainwater - Hard/trapping_rainwater.py
@@ -0,0 +1,15 @@
+class Solution:
+    def trap(self, height: list[int]) -> int:
+        local_max = 0
+        step = 0
+        for index, value in enumerate(height):
+            
+
+
+        height_set = set(height)
+        current_height = max(height_set)
+        while current_height > 0:
+            if current_height not in height_set:
+                current_height -=1
+                continue
+            
